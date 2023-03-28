459564aa4f47c113a855452af071af58edde4639
Madihander <79453431+Madihander@users.noreply.github.com>
2023-03-28 16:16:33 +0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <SignUpForm/>
 </template>
 
+<script>
+import SignUpForm from '@/components/SignUpForm.vue';
+
+export default {
+  components:{SignUpForm},
+  data() {
+    return {
+    }
+  },
+  methods:{
+  }
+}
+
+
+</script>
+
 <style>
+body{
+  margin:0;
+  background-color: #eee;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir,Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
